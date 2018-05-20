753ca29971630e79ff5bb4f6de4d323193785e73
Julio Lozovei <jlozovei@gmail.com>
2018-05-19 23:35:57 -0300

    add icons and progressive image plugins
    add plugins reference in vue instance

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+import progressiveImage from 'vue-progressive-image'
+
+Vue.use(progressiveImage)
+Vue.component('icon', Icon)
+
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app')
